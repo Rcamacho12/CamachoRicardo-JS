9b98fe6d19158a37d1e66996c54236e621b1f67a
Ricardo <camacho.ricardocl@gmail.com>
2024-09-17 19:17:11 -0300

    limpiando codidigo

--- js/main.js
+++ js/main.js
@@ -20,95 +20,17 @@ class ArticuloLibreria {
     }
 }
 
-const articuloLibreria1 = new ArticuloLibreria("Cuaderno A4", 4000);
-const articuloLibreria2 = new ArticuloLibreria("Cuaderno A3", 12000);
-const articuloLibreria3 = new ArticuloLibreria("Lapicera Azul x12", 3600);
-const articuloLibreria4 = new ArticuloLibreria("Lapicera Negra x12", 3600);
+const articuloLibreria1 = new ArticuloLibreria("cuaderno tapa blanda", 12950);
+const articuloLibreria2 = new ArticuloLibreria("cuaderno ejecutivo", 13400);
+const articuloLibreria3 = new ArticuloLibreria("cuaderno Univercitario", 18000);
+const articuloLibreria4 = new ArticuloLibreria("cuaderno tapa flexible", 3750);
+const articuloLibreria5 = new ArticuloLibreria("cuaderno tapa plastica", 7500);
+const articuloLibreria6 = new ArticuloLibreria("cuaderno escolar", 3750);
+const articuloLibreria7 = new ArticuloLibreria("cuaderno escolar con lunares", 3750);
+const articuloLibreria8 = new ArticuloLibreria("cuaderno ecologico" , 7250);
 
-const listaArticuloLibreria = [articuloLibreria1, articuloLibreria2, articuloLibreria3, articuloLibreria4];
-const listaLibros = [productoLibro1, productoLibro2, productoLibro3, productoLibro4];
-
-// function saludarUsuario(nombre) {
-//     alert("¡Bienvenido, " + nombre + "!");
-// }
-
-// function categoriaProducto() {
-//     let mensaje = "Selecciona un producto ingresando el número correspondiente:\n";
-//     mensaje += "1. Libros\n";
-//     mensaje += "2. Artículos de librería\n";
-//     let seleccion;
-//     while (true) {
-//         seleccion = prompt(mensaje);
-//         if (seleccion === "1") {
-//             categoriaLibros();
-//             break;
-//         } else if (seleccion === "2") {
-//             categoriaArticulosLibreria();
-//             break;
-//         } else {
-//             alert("Selección no válida. Por favor, intenta de nuevo.");
-//         }
-//     }
-// }
-
-// function categoriaLibros() {
-//     let mensaje = "Elegi el numero del Libro que estás buscando:\n";
-    
-//     for (let i = 0; i < listaLibros.length; i++) {
-//         mensaje += (i + 1) + ". " + listaLibros[i].nombre + "\n";
-//     }
-//     mensaje += (listaLibros.length + 1) + ". Salir\n";
-    
-//     let seleccion;
-//     let productoSeleccionado;
-//     while (true) {
-//         seleccion = prompt(mensaje);
-//         let seleccionNumero = parseInt(seleccion);
-        
-//         if (seleccionNumero > 0 && seleccionNumero <= listaLibros.length) {
-//             productoSeleccionado = listaLibros[seleccionNumero - 1].nombre;
-//             break;
-//         } else if (seleccionNumero === listaLibros.length + 1) {
-//             productoSeleccionado = "Salir";
-//             break;
-//         } else {
-//             alert("Selección no válida. Por favor, intenta de nuevo.");
-//         }
-//     }
-    
-//     alert("Has seleccionado: " + productoSeleccionado);
-// }
-
-// function categoriaArticulosLibreria() {
-//     let mensaje = "Elegi el numero del Artículo de librería que estás buscando:\n";
-    
-//     for (let i = 0; i < listaArticuloLibreria.length; i++) {
-//         mensaje += (i + 1) + ". " + listaArticuloLibreria[i].nombre + "\n";
-//     }
-//     mensaje += (listaArticuloLibreria.length + 1) + ". Salir\n";
-    
-//     let seleccion;
-//     let productoSeleccionado;
-//     while (true) {
-//         seleccion = prompt(mensaje);
-//         let seleccionNumero = parseInt(seleccion);
-        
-//         if (seleccionNumero > 0 && seleccionNumero <= listaArticuloLibreria.length) {
-//             productoSeleccionado = listaArticuloLibreria[seleccionNumero - 1].nombre;
-//             break;
-//         } else if (seleccionNumero === listaArticuloLibreria.length + 1) {
-//             productoSeleccionado = "Salir";
-//             break;
-//         } else {
-//             alert("Selección no válida. Por favor, intenta de nuevo.");
-//         }
-//     }
-    
-//     alert("Has seleccionado: " + productoSeleccionado);
-// }
+const listaArticuloLibreria = [articuloLibreria1, articuloLibreria2, articuloLibreria3, articuloLibreria4, 
+                                articuloLibreria5, articuloLibreria6, articuloLibreria7, articuloLibreria8];
 
 
 
-// let nombreUsuario = prompt("Por favor, ingresa tu nombre: ");
-// saludarUsuario(nombreUsuario);
-// categoriaProducto();
